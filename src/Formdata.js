a5fe2ee12e31beb686bac0178cc312865c27e784
Usama <usama.wizz@gmail.com>
2022-12-08 10:49:09 +0500

    Show data in table and empty input fields works

--- src/Formdata.js
+++ src/Formdata.js
@@ -2,7 +2,16 @@ import React, {useState} from 'react'
 
 function Formdata(props) {
 
-  const [handle,setHandle] = useState();
+
+  const empty = {
+    id : null,
+    name : '',
+    email : '',
+    phone : '',
+
+  };
+
+  const [handle,setHandle] = useState(empty);
   const handleInput = (event) =>{
     const {name , value} = event.target;
     setHandle({...handle,[name]:value});
@@ -14,16 +23,17 @@ function Formdata(props) {
         <form onSubmit={(e)=>{
           e.preventDefault();
           props.myuser(handle);
+          setHandle(empty);
         }}
         >
             <label>Name</label>
-            <input onChange={handleInput} type="text" className = "form-control shadow" name = "name" />
+            <input onChange={handleInput} type="text" className = "form-control shadow" name = "name" value = {handle.name}/>
 
             <label>Email</label>
-            <input onChange={handleInput} type="email" className = "form-control shadow" name = "email" />
+            <input onChange={handleInput} type="email" className = "form-control shadow" name = "email" value = {handle.email} />
 
             <label>Phone</label>
-            <input onChange={handleInput} type="number" className = "form-control shadow" name = "phone"/>
+            <input onChange={handleInput} type="number" className = "form-control shadow" name = "phone" value = {handle.phone}/>
 
             <input  type= "submit" className='btn btn-outline-primary mt-5' name = "phone" />
 
